5955cf70572bcc678944a0f1a7281a00134a1c5d
MathieuCarteron <carteronmathieu@gmail.com>
2026-05-03 18:38:31 +0200

    ajout des images du 2eme stage

--- js/page.js
+++ js/page.js
@@ -50,9 +50,7 @@ document.addEventListener("DOMContentLoaded", function() {
     desc: "Création d'une application de cartes interactives, on choisit une matière puis on peut réviser avec différents thèmes.",
     tags: ["HTML", "CSS", "JS", "PHP", "MySQL"],
     images: [
-      { src: "img/projets/memory1.png", caption: "Page d'accueil – choix de la matière" },
-      { src: "img/projets/memory2.png", caption: "Vue des cartes à retourner" },
-      { src: "img/projets/memory3.png", caption: "Résultat de la session" }
+      { src: "img/projets/memory1.png", caption: "Vue globale" },
     ]
   },
   {
@@ -64,7 +62,6 @@ document.addEventListener("DOMContentLoaded", function() {
     tags: ["HTML", "JavaScript", "PgSQL"],
     images: [
       { src: "img/projets/kanban1.png", caption: "Vue générale du tableau" },
-      { src: "img/projets/kanban2.png", caption: "Ajout d'une tâche" }
     ]
   },
   {
@@ -75,8 +72,8 @@ document.addEventListener("DOMContentLoaded", function() {
     desc: "Gestion des différents acteurs de Pyrofètes : création d'une API RESTful en C# et d'un front-end Angular permettant de gérer les clients, les prestataires et les différentes opportunités.",
     tags: ["C#", "API RESTful", "Angular"],
     images: [
-      { src: "img/projets/pyro1.png", caption: "Dashboard principal" },
-      { src: "img/projets/pyro2.png", caption: "Gestion des intervenants" }
+      { src: "img/projets/pyro1.png", caption: "Gestion des clients" },
+      { src: "img/projets/pyro2.png", caption: "Page opportunités" }
     ]
   },
   {
@@ -87,7 +84,7 @@ document.addEventListener("DOMContentLoaded", function() {
     desc: "Développement d'une application mobile permettant des discussions chiffrées entre membres et par groupes. API RESTful en C#, front-end Angular transformé en application native Android via Capacitor.",
     tags: ["C#", "API RESTful", "Angular", "Capacitor"],
     images: [
-      { src: "img/projets/chat1.png", caption: "Liste des conversations" },
+      { src: "img/projets/chat1.png", caption: "Liste des utilisateurs" },
       { src: "img/projets/chat2.png", caption: "Vue d'une discussion chiffrée" }
     ]
   },
@@ -111,10 +108,10 @@ document.addEventListener("DOMContentLoaded", function() {
     desc: "Développement de 3 projets C# .NET 9 en collaboration avec un autre stagiaire. Ces applications automatisent la préparation à l'arrivée et au départ des agents : déplacement de données d'une API à un DataLake puis de ce dernier vers un DataWareHouse, le dernier projet prépare les différentes actions qui seront effectuées lors de l'arrivée/départ de l'employé/prestataire.",
     tags: ["C#", ".NET 9", "HTTP Client", "xUnit", "FluentAssertions", "FakeItEasy"],
     images: [
-      { src: "img/projets/stage2-1", caption: "Vue globale premier projet" },
-      { src: "img/projets/stage2-2", caption: "Vue globale deuxième projet" },
-      { src: "img/projets/stage2-3", caption: "Vue globale troisième projet" },
-      { src: "img/projets/stage2-4", caption: "Vue globale tests" }
+      { src: "img/projets/stage2-1.png", caption: "Vue globale premier projet" },
+      { src: "img/projets/stage2-2.png", caption: "Vue globale deuxième projet" },
+      { src: "img/projets/stage2-3.png", caption: "Vue globale troisième projet" },
+      { src: "img/projets/stage2-4.png", caption: "Exemple Test Unitaire" }
     ]
   }
   
